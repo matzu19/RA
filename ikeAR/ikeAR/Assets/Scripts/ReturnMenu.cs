448b8a9744d6ee9eb2375e0904c77ac5f3e27890
matzu19 <mateo.zuluagag@upb.edu.co>
2021-10-02 13:44:20 -0500

    retunr

--- ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
+++ ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ReturnMenu : MonoBehaviour
 {
+    [SerializeField] GameObject Colores;
+    [SerializeField] GameObject Object;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,17 @@ public class ReturnMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+    public void Return()
+    {
+      if (!Colores.activeInHierarchy)
+        {
+            Colores.SetActive(true);
+            Object.SetActive(false);
+        }
+        
+
         
     }
 }
