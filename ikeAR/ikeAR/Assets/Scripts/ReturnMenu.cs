4f31f81969d3e5d63c05a53c66fba4ffc2e7abe4
FatalitySMG <santiagosmg00@hotmail.com>
2021-10-02 15:23:53 -0400

    .

--- ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
+++ ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
@@ -7,17 +7,6 @@ public class ReturnMenu : MonoBehaviour
     [SerializeField] GameObject Colores;
     [SerializeField] GameObject Object;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void Return()
     {
       if (!Colores.activeInHierarchy)
