92ef4c0660724ac94507e69b98ec4c897ba1bca6
FatalitySMG <santiagosmg00@hotmail.com>
2021-10-02 18:52:19 -0400

    Listo

--- ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
+++ ikeAR/ikeAR/Assets/Scripts/ReturnMenu.cs
@@ -6,7 +6,14 @@ public class ReturnMenu : MonoBehaviour
 {
     [SerializeField] GameObject Colores;
     [SerializeField] GameObject Object;
+    [SerializeField] GameObject ObjectChild;
+    private Vector3 xl = new Vector3(10f, 10f, 10f), xd = new Vector3(0, 0, -15f), Ol, Od;
 
+    public void Start()
+    {
+        Ol = this.ObjectChild.transform.localScale;
+        Od = this.ObjectChild.transform.rotation.eulerAngles;
+    }
     public void Return()
     {
       if (!Colores.activeInHierarchy)
@@ -14,8 +21,23 @@ public class ReturnMenu : MonoBehaviour
             Colores.SetActive(true);
             Object.SetActive(false);
         }
-        
-
-        
+        this.ObjectChild.transform.localScale = Ol;
+        this.ObjectChild.transform.rotation = Quaternion.Euler(Od);
+    }
+    public void TurnRight()
+    {
+        this.ObjectChild.transform.Rotate(-xd, Space.Self);
+    }
+    public void TurnLeft()
+    {
+        this.ObjectChild.transform.Rotate(xd, Space.Self);
+    }
+    public void ZoomIn()
+    {
+        this.ObjectChild.transform.localScale += xl;
+    }
+    public void ZoomOut()
+    {
+        this.ObjectChild.transform.localScale -= xl;
     }
 }
